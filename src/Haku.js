f5735af48f4a994668b541e1d35a700a94a6d7bf
Aija Koivistoinen <aija.koivistoinen@edu.savonia.fi>
2022-05-11 23:44:40 +0300

    Nimellä haku

--- src/Haku.js
+++ src/Haku.js
@@ -1,6 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
+let haku = " ";
+let nimi_haku = " ";
+
+function NimiHaku(props) {
+  const [nimi, setNimi] = useState("");
+  const [loading, setLoading] = useState("");
+  const [tuotteet, setTuotteet] = useState([]);
+
+  async function fetchData() {
+    haku = "";
+    if(nimi != "") {
+      nimi_haku = "?nimi=" + nimi;
+      haku = nimi_haku;
+    }
+    setLoading("Lataa...");
+    setTuotteet([]);
+    let response = await fetch ("http://localhost:4000/tuote" + haku);
+    let data = await response.json();
+    setTuotteet(data);
+    setLoading(" ");
+    if(data === "") {
+      setLoading("Ei hakutuloksia");
+    }
+
+  }
+}
 
 export default function Haku() {
   return (
@@ -15,7 +42,7 @@ export default function Haku() {
         <Link to="/Haku">Hyllytiedot</Link> |{" "}
         <Link to="/Tuote">Tuotteet</Link>
       </nav>
-      <h1>Tähän haetaan kaikki hyllytiedot</h1>
+      <NimiHaku />
     </div>
   );
 }
